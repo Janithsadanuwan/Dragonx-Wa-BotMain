ec4f25f8b0edd4eb0ba67de509be57d753388901
Janith sadanuwan/ <95123183+Janithsadanuwan@users.noreply.github.com>
2022-02-11 10:59:36 +0530

    Dragonx

--- plugins/Tlpackpub.js
+++ plugins/Tlpackpub.js
@@ -1,4 +1,4 @@
-janithconst Trex = require('../events');
+const Trex = require('../events');
 const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
 const fs = require('fs');
 const axios = require('axios');
